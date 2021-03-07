5ad227bf774390285dfc70f59f21dec32c70bb59
Franco Méndez <f@francomendez.com>
2021-03-07 05:09:03 -0300

    feat: add redirect endpoint

---
+++ pages/api/redirect.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const url = req.query["url"] as string;
+  res.redirect(307, url);
+}
