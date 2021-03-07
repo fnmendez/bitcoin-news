e6c05e7456b8f08f064281ad05326cf26be0296b
Franco Méndez <f@francomendez.com>
2021-03-07 05:08:18 -0300

    feat: add parse mode

--- libs/telegram.ts
+++ libs/telegram.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const token = "1500529848:AAEhZp_IpYbrjGzOnbPBf1scLOZxdH_VB1g";
-const chatIds = ["-487210527"];
+const chatIds = ["-1001407421921"];
 
 const client = axios.create({
   baseURL: `https://api.telegram.org/bot${token}`,
@@ -16,6 +16,7 @@ export const sendMessage = async (text: string) => {
     const message = await client.post("sendMessage", {
       chat_id: chatId,
       text,
+      parse_mode: "HTML",
     });
     if (message) sentMessages += 1;
   }
