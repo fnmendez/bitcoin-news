da5dbf7356db98904332909470ebfcee25e0c6d3
Franco Méndez <f@francomendez.com>
2021-03-09 11:17:55 -0300

    feat: add ga tracking to landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
+import dedent from "dedent";
+import Head from "next/head";
+
 export default function HomePage() {
   return (
     <>
-      <h1>What are you doing here? Stack sats!</h1>
-      <p>(Not hiring)</p>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: dedent`
+              <!-- Global site tag (gtag.js) - Google Analytics -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9KCHRTG83"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-N9KCHRTG83');
+              </script>
+              `,
+          }}
+        />
+      </Head>
+      <div>
+        <h1>What are you doing here? Stack sats!</h1>
+        <p>(Not hiring)</p>
+      </div>
     </>
   );
 }
