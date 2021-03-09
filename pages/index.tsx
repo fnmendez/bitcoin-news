729fa8bd3d2a2d10fdb86de352c143571aef448d
Franco Méndez <f@francomendez.com>
2021-03-09 11:23:33 -0300

    fix: ga integrations

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,14 @@ export default function HomePage() {
   return (
     <>
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9KCHRTG83"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: dedent`
-              <!-- Global site tag (gtag.js) - Google Analytics -->
-              <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9KCHRTG83"></script>
-              <script>
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-N9KCHRTG83');
-              </script>
               `,
           }}
         />
