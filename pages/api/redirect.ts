747f2564b69317c0496c1e0de1f12ab662bafe03
Franco Méndez <f@francomendez.com>
2021-03-11 03:10:55 -0300

    chore: delete unuseful route

--- pages/api/redirect.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const url = req.query["url"] as string;
-  res.redirect(301, url);
-}
