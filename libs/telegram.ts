aa4d8aa767f0f766f45be064e9dd0b5b5f451e33
Franco Méndez <f@francomendez.com>
2021-03-13 01:39:27 -0300

    feat: add logger chat

--- libs/telegram.ts
+++ libs/telegram.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const token = process.env.TELEGRAM_TOKEN;
-const chatIds = ["-1001407421921"];
+const mainChatId = "-1001407421921";
+const hqChatId = "-535034198";
 
 const client = axios.create({
   baseURL: `https://api.telegram.org/bot${token}`,
@@ -9,16 +10,29 @@ const client = axios.create({
 });
 
 export const sendMessage = async (text: string, preview = false, silent = false) => {
-  let sentMessages = 0;
-  for (const chatId of chatIds) {
+  try {
     const message = await client.post("sendMessage", {
-      chat_id: chatId,
+      chat_id: mainChatId,
       text,
       parse_mode: "HTML",
       disable_web_page_preview: !preview,
       disable_notification: silent,
     });
-    if (message) sentMessages += 1;
+    const success = Boolean(message);
+    return success;
+  } catch (err) {
+    await sendLog(`Failed to send message: ${err.name}\n\`\`\`${err.stack}\`\`\``, false);
+    return false;
   }
-  return chatIds.length === sentMessages;
+};
+
+export const sendLog = async (text: string, silent: boolean) => {
+  const message = await client.post("sendMessage", {
+    chat_id: hqChatId,
+    text,
+    parse_mode: "HTML",
+    disable_web_page_preview: true,
+    disable_notification: silent,
+  });
+  return Boolean(message);
 };
