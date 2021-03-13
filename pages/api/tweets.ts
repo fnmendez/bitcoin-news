75fb83bf7903380612fc951947bd3ce2a4559de1
Franco Méndez <f@francomendez.com>
2021-03-13 12:23:01 -0300

    feat: if not production do not store tweets

--- pages/api/tweets.ts
+++ pages/api/tweets.ts
@@ -6,8 +6,10 @@ import { getRecentTweets, tweetToMessage } from "~/libs/twitter";
 import { Tweet } from "~/src/types";
 import { CHUNK_ARRAY } from "~/src/utils";
 
+const DEV = process.env.VERCEL_ENV !== "production";
+
 async function saveTweets(tweets: Tweet[]) {
-  if (!tweets?.length) return;
+  if (!tweets?.length || DEV) return;
   const batches = CHUNK_ARRAY(tweets, 25);
   for (const batch of batches) {
     const params = {
