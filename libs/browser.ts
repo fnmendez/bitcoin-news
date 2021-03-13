293ba7968afe16c3dd1035511432ae8c58d15877
Franco Méndez <f@francomendez.com>
2021-03-13 12:32:41 -0300

    feat: use original browser library

--- libs/browser.ts
+++ libs/browser.ts
@@ -1,26 +1,43 @@
 import chrome from "chrome-aws-lambda";
-import puppeteer from "puppeteer";
 import core from "puppeteer-core";
 
-let _browser: core.Browser | puppeteer.Browser | null;
+let _browser: core.Browser | null;
 
-export async function getBrowser(isLocal: boolean) {
+export async function getBrowser(isDev: boolean) {
   if (!_browser) {
-    const options = await getOptions();
-    if (isLocal) {
-      _browser = await puppeteer.launch();
-    } else {
-      _browser = await core.launch(options);
-    }
+    const options = await getOptions(isDev);
+    _browser = await core.launch(options);
   }
   return _browser;
 }
 
-export async function getOptions() {
-  const options = {
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-  };
+const exePath =
+  process.platform === "win32"
+    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    : process.platform === "linux"
+    ? "/usr/bin/google-chrome"
+    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+
+interface Options {
+  args: string[];
+  executablePath: string;
+  headless: boolean;
+}
+
+export async function getOptions(isDev: boolean) {
+  let options: Options;
+  if (isDev) {
+    options = {
+      args: [],
+      executablePath: exePath,
+      headless: true,
+    };
+  } else {
+    options = {
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    };
+  }
   return options;
 }
