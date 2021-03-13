b77064b5eb4f688d59fc26d53cd10371a64b9725
Franco Méndez <f@francomendez.com>
2021-03-13 14:59:42 -0300

    feat: improve log

--- pages/api/tweets.ts
+++ pages/api/tweets.ts
@@ -80,7 +80,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.setHeader("Content-Type", "text/html");
     return res.end("ok");
   } catch (err) {
-    sendLog({ text: `[tweets] Error on handler: ${err.name}\n\`\`\`${err.stack}\`\`\``, silent: false });
+    sendLog({ text: `[tweets] Error on handler: ${err.name}\n<pre>${err.stack}</pre>`, silent: false });
     res.statusCode = 500;
     res.setHeader("Content-Type", "text/html");
     return res.end(err.stack);
