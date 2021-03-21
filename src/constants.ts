2f767b2570d417baf07457e383acdf65a472fb77
Franco Méndez <f@francomendez.com>
2021-03-21 20:37:55 -0300

    feat: ban speculative title

--- src/constants.ts
+++ src/constants.ts
@@ -17,4 +17,4 @@ export const SOURCE_NAME_BLACKLIST = [
   "inside bitcoins",
   "obn",
 ];
-export const TITLE_BLACKLIST = ["ripple", "xrp", "cointelegraph", "nft", "price analysis"];
+export const TITLE_BLACKLIST = ["ripple", "xrp", "cointelegraph", "nft", "price analysis", "price prediction"];
