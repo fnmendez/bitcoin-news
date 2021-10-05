7355ae78abed0d4875859cca314ede7f35af144a
Franco Méndez <f@francomendez.com>
2021-10-05 18:24:31 -0300

    feat: Add Slack library for sending messages

---
+++ libs/slack.ts
@@ -0,0 +1,19 @@
+const webhookURL = process.env.SLACK_WEBHOOK!;
+
+export const sendMessage = async (text: string): Promise<boolean> => {
+  try {
+    const res = await fetch(webhookURL, {
+      method: "POST",
+      body: JSON.stringify({ text }),
+      headers: { ["content-type"]: "application/json" },
+    });
+    const success = res.status >= 200 && res.status < 300;
+    if (!success) {
+      console.log(`Failed to send message\n<pre>${text}</pre>`);
+    }
+    return success;
+  } catch (err: any) {
+    console.log(`Failed to send message: ${err.name}\n<pre>${err.stack}</pre>`);
+    return false;
+  }
+};
