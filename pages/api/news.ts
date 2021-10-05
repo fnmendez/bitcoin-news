7016be0e622c5f16326c869847eff2f88faf284f
Franco Méndez <f@francomendez.com>
2021-10-05 18:24:17 -0300

    feat: Send news to Slack

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -2,7 +2,8 @@ import cheerio from "cheerio";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { batchWrite as dynamodbBatchWrite, batchGet as dynamodbBatchGet } from "~/libs/dynamodb";
-import { sendLog, sendMessage } from "~/libs/telegram";
+import * as slack from "~/libs/slack";
+import * as telegram from "~/libs/telegram";
 import { headers } from "~/src/constants";
 import { Instruction, News } from "~/src/types";
 import { HUMAN_TIME, SAFE_HTML as SH, TIMESTAMP, BLACKLISTED, TODAY_TIME } from "~/src/utils";
@@ -15,7 +16,7 @@ async function saveBitcoinNews(news: News[]) {
   for (const batch of batches) {
     const params = {
       RequestItems: {
-        "bitcoin-news-titles": batch.map((n) => ({
+        "bitcoin-news-titles-2": batch.map((n) => ({
           PutRequest: {
             Item: {
               title: SAFE_TITLE_KEY(n.title),
@@ -24,6 +25,7 @@ async function saveBitcoinNews(news: News[]) {
               source_name: n.sourceName,
               published_at: n.publishedAt,
               timestamp: n.timestamp,
+              live_until: Number(String(new Date().getTime()).slice(0, -3)) + 86_400, // one day
             },
           },
         })),
@@ -38,11 +40,11 @@ async function getBitcoinNews(news: News[]) {
   if (!news?.length) return [];
   const params = {
     RequestItems: {
-      "bitcoin-news-titles": { Keys: news.map((n) => ({ ["title"]: SAFE_TITLE_KEY(n.title) })) },
+      "bitcoin-news-titles-2": { Keys: news.map((n) => ({ ["title"]: SAFE_TITLE_KEY(n.title) })) },
     },
   };
   const res = await dynamodbBatchGet(params);
-  return res.Responses?.["bitcoin-news-titles"] || [];
+  return res.Responses?.["bitcoin-news-titles-2"] || [];
 }
 
 async function filterNews(news: News[]): Promise<News[]> {
@@ -50,21 +52,23 @@ async function filterNews(news: News[]): Promise<News[]> {
   const toFilter = storedNews.map((n) => n["title"]);
   const filtered = news.filter((n) => !toFilter.includes(n.link) && !toFilter.includes(n.title));
   console.log(`[news] got: ${news.length} fresh: ${filtered.length}`);
-  // sendLog({
+  // telegram.sendLog({
   //   text: `[news] got: ${news.length} fresh: ${filtered.length}`,
   //   silent: false,
   // });
   return filtered;
 }
 
-async function sendNewsToTelegram(news: News[]): Promise<boolean> {
+async function sendNewsToTelegramAndSlack(news: News[]): Promise<boolean> {
   const ordered = news.sort((a, b) => a.timestamp - b.timestamp);
   const batches = CHUNK_ARRAY(ordered, 8);
   let success = true;
   for (const batch of batches) {
     const text = batch.map((n) => n.text).join("\n\n");
-    const ok = await sendMessage({ text: text, silent: true });
-    success = success && ok;
+    const textSlack = batch.map((n) => n.textSlack).join("\n\n");
+    const okTelegram = await telegram.sendMessage({ text: text, silent: true });
+    const okSlack = await slack.sendMessage(textSlack);
+    success = success && okTelegram && okSlack;
     await saveBitcoinNews(batch);
     await new Promise((r) => setTimeout(r, 800));
   }
@@ -78,13 +82,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const news = GOOGLE_NEWS.cheerioProcess(html);
     const filteredNews = await filterNews(news);
     if (filteredNews && filteredNews.length) {
-      await sendNewsToTelegram(filteredNews);
+      await sendNewsToTelegramAndSlack(filteredNews);
     }
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     return res.end("ok");
-  } catch (err) {
-    sendLog({ text: `[news] Error on handler: ${err.name}\n\`\`\`${err.stack}\`\`\``, silent: false });
+  } catch (err: any) {
+    telegram.sendLog({ text: `[news] Error on handler: ${err.name}\n\`\`\`${err.stack}\`\`\``, silent: false });
     res.statusCode = 500;
     res.setHeader("Content-Type", "text/html");
     return res.end(err.stack);
@@ -111,6 +115,7 @@ const GOOGLE_NEWS: Instruction = {
           publishedAt: humanTime,
           timestamp: TIMESTAMP(time),
           text: `📰 <b>${SH(title)}</b>\n${SH(sourceName)}\n${SH(todayTime)} - <a href='${sourceUrl}'>Read article</a>`,
+          textSlack: `📰 *${SH(title)}*\n${SH(sourceName)}\n${SH(todayTime)} - <${sourceUrl}|Read article>`,
           keywords,
         });
       }
